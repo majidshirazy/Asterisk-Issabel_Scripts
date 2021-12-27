7732e50f389aec97e2d8c8f8aee18f97efc46361
Majid Shiraazi <majidshirazy@gmail.com>
2021-12-28 00:21:17 +0330

    Update WAV2MP3.sh
    Add url fix for outgoing calls in database

--- WAV2MP3.sh
+++ WAV2MP3.sh
@@ -28,3 +28,14 @@ do
         exit 1
     fi
 done
+
+for outwav in `mysql -u $db_user -p$db_password $db_name<<<"select recordingfile from cdr where recordingfile not like '/var/spool/%' AND calldate < DATE_SUB(CURDATE(), INTERVAL 1 DAY) ORDER BY calldate desc"`
+do
+    date=$(echo $outwav | cut -d'-' -f4)
+    outmp3="$(echo $outwav | sed s/".wav"/".mp3"/)"
+    year=${date:0:4}
+    month=${date:4:2}
+    day=${date:6:2}
+    mysql -u $db_user -p$db_password $db_name<<<"UPDATE cdr SET recordingfile='/var/spool/asterisk/monitor/$year/$month/$day/$outmp3' WHERE recordingfile = '$outwav'"
+
+done
